93e6dc21848aee478fff5ce76c7656c52b800a49
NikhilDongardive1404 <106867939+NikhilDongardive1404@users.noreply.github.com>
2025-06-19 19:46:36 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char main()
+int main()
 {
-    char a;
-    printf("Enter the name: ");
-    scanf("%c",&a);
-    printf("The name is %c",a);
-    printf("\nThe name is stored at %d",&a);
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    printf("Enter the Numbers: \n");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    c = a + b;
+    d = a - b;
+    e = a*b;
+    f = a/b;
+    g = a%b;
+    printf("The addition of the number is %d\n",c);
+    printf("The substraction of the number is %d\n",d);
+    printf("The multiplication of the number is %d\n",e);
+    printf("The division of the number is %d\n",f);
+    printf("The modulus of the number is %d\n",g);
     return 0;
 }
